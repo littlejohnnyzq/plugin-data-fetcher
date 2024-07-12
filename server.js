bbf7c685d3d79ded48737fa823de98d45ce4eec4
littlejohnnyzq <littlejohnnyzq@icloud.com>
2024-07-12 11:56:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -182,6 +182,6 @@ async function autoScroll(page){
     });
 }
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(3389, '121.40.69.104', () => {
+    console.log(`Server is running on http://121.40.69.104:3389`);
 });
